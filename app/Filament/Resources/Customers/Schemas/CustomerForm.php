595bfb3f3c5dbcf480b54c515b02e77b5d46f13e
Maksim A. Sheptookha <rellikso@gmail.com>
2025-09-30 09:53:57 +0300

    feat/add-customer-entity: done.

---
+++ app/Filament/Resources/Customers/Schemas/CustomerForm.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filament\Resources\Customers\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class CustomerForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('phone')
+                    ->tel()
+                    ->required(),
+                TextInput::make('email')
+                    ->label('Email address')
+                    ->email()
+                    ->required(),
+            ]);
+    }
+}
